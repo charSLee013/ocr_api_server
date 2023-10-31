0f29fd42d633a538fb48acfa80e68397dfde568e
charSLee <735365895@qq.com>
2023-10-31 22:29:47 +0800

    fix: not module name is pip3

--- start_local.sh
+++ start_local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install deps
-python3 -m pip3 install --no-cache-dir -q -r requirements.txt
+python3 -m pip install --no-cache-dir -q -r requirements.txt
 sudo apt-get --allow-releaseinfo-change update -qq && apt install -qq libgl1-mesa-glx libglib2.0-0 -y
 
 # start local
